b3df247f6fc21a8c3c10f3093d0e67ed1fcd94f2
vinileoml <viniciusleomoraisdelima@gmail.com>
2024-08-21 20:46:30 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer IdadePessoa = 20;
+        String NomePessoa = "Rendrik";
+
+        /*
+        * Operadores Logicos
+        * > Maior
+        * >= Maior igual
+        * < Menor
+        * <= Menor igual
+        * == é igual
+        * != é diferente
+        * é qual compara textos
+         */
+        if (IdadePessoa > 20) {
+            System.out.println("é maior que 20");
+        }else{
+            System.out.println("não é maior que 20");
+        }
     }
 }
